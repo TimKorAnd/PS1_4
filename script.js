81033fed33a15089b22023bc9a83dd86d1121d50
TimKorAnd <timkorand@gmail.com>
2019-04-09 01:38:52 +0300

    form1 inputs validation oop

--- script.js
+++ script.js
@@ -1,80 +1,73 @@
 /* CONSTANTS */
 
-const EMAIL_VALID_REGEX = /^(\w+(-(?=\w))?\w*)@\w+\.(\w+(-(?=\w))?\w+)$/;
+const EMAIL_VALID_REGEXP = /^(\w+(-(?=\w))?\w*)@\w+\.(\w+(-(?=\w))?\w+)$/;
 const PASSWORD_VALID_REGEX = /^(\w|\W){8,}$/;
 const NOT_EMPTY_VALID = /^.+$/;
 
-/*bind a event listener with specified func to each from array of DOM elements*/
-function eventLoader(action, func, elementsId, options) {
-    elementsId.forEach((value)=>{
-        let el = document.getElementById(value);
-        el.addEventListener(action, func, options);
-    });
+
+/*class for input*/
+class InputField {
+    constructor(inputId, VALID_REGEXP){
+    this.inputId = inputId;
+    this.element = document.getElementById(this.inputId);
+    this.VALID_REGEXP = VALID_REGEXP;
+    }
+
+    isValid() {
+            return this.VALID_REGEXP.test(this.element.value);
+    };
+
+    viewValidation() {
+            let statusSearch = '--valid', statusReplace = '--invalid';
+            if (this.isValid()) {
+                [statusSearch, statusReplace] = [statusReplace, statusSearch];
+            };
+
+            console.log(this.element.className);
+            this.element.className = this.element.className.replace(statusSearch, statusReplace);
+        };
+}
+
+function nextForm(btnForm1, emailInput, passwordInput) {
+    console.log('btn    ');
+    if (emailInput.isValid() && passwordInput.isValid()) {
+        /* btnForm1.setAttribute('disabled', 'false');*/
+        console.log('btn  ok  ');
+        return true;
+    } else {
+        console.log('btn    close');
+        return false;
+    }
 }
 
 function start(){
-
-    eventLoader('blur', () => {
-        registration('right-panel__form-1__email',
-        'right-panel__form-1__password',
-        'remember-me-checkbox','right-panel__form-1__submit-button');
-        eventLoader('input', () => registration('right-panel__form-1__email',
-            'right-panel__form-1__password',
-            'remember-me-checkbox','right-panel__form-1__submit-button'),
-            ['right-panel__form-1__email'],{once:false});
-        },
-        ['right-panel__form-1__email'],{once:true});
-
-    eventLoader('blur', () => {
-            registration('right-panel__form-1__email',
-                'right-panel__form-1__password',
-                'remember-me-checkbox','right-panel__form-1__submit-button');
-            eventLoader('input', () => registration('right-panel__form-1__email',
-                'right-panel__form-1__password',
-                'remember-me-checkbox','right-panel__form-1__submit-button'),
-                ['right-panel__form-1__password'],{once:false});
-        },
-        ['right-panel__form-1__password'],{once:true});
-
-
-    eventLoader('click', () => registration('right-panel__form-1__email',
-        'right-panel__form-1__password',
-        'remember-me-checkbox','right-panel__form-1__submit-button'),
-        ['right-panel__form-1__submit-button'],{once:false});
-}
-
-
-/* return true if input valid = match regExp, false if not */
-function isValid(formInputsObj){
-    function showInputValid(validness,inputId) {
-        const elem = document.getElementById(inputId);
-        if (validness){
-            elem.className = 'right-panel__input block-element'
-        } else {
-            elem.className = 'right-panel__input--invalid block-element';
+    /*get form1 DOM elems */
+    let emailInput = new InputField('right-panel__form-1__email', EMAIL_VALID_REGEXP);
+    emailInput.element.addEventListener('blur', () =>  {
+            emailInput.viewValidation();
+            emailInput.element.addEventListener('input', emailInput.viewValidation.bind(emailInput),{once:false});
         }
-    }
-    let ret = true;
-    formInputsObj.forEach((currObj)=>{
-        let result;
-        showInputValid(result = currObj.regTemplate.test(document.getElementById(currObj.inputId).value), currObj.inputId);
-        if (ret) {
-            ret = result;
-        };
-    });
-    return ret;
-}
+        ,{once:true});
 
-function registration(rightPanelForm1Email, rightPanelForm1Password, rememberMeCheckbox,rightPanelForm1SubmitButton) {
+    let passwordInput = new InputField('right-panel__form-1__password', PASSWORD_VALID_REGEX);
+    passwordInput.element.addEventListener('blur', () =>  {
+            passwordInput.viewValidation();
+            passwordInput.element.addEventListener('input', passwordInput.viewValidation.bind(passwordInput),{once:false});
+        }
+        ,{once:true});
 
-    if (!isValid([{regTemplate:EMAIL_VALID_REGEX,inputId:rightPanelForm1Email},
-        {regTemplate:PASSWORD_VALID_REGEX,inputId:rightPanelForm1Password}])){
-        console.log('isValid false');
-        /*invalidInputsShow();*/
-        return;
+    const btnForm1 = document.getElementById('right-panel__form-1__submit-button');
+    /*btnForm1.disabled = true;*/
+    /*btnForm1.addEventListener('submit', () => nextForm(btnForm1, emailInput, passwordInput));*/
 
-    } else {
-        console.log('isValid true');
-    }
+    const form1 = document.getElementById('right-panel__form-1');
+    form1.addEventListener('submit',() => nextForm(btnForm1, emailInput, passwordInput));
+
+
 
 }
+
+
+
+
+
