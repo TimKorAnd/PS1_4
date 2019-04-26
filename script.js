91fee80578f55dd04a5011c64cb11839a4478a65
TimKorAnd <timkorand@gmail.com>
2019-04-26 06:09:52 +0300

    for mentor's review

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 const EMAIL_VALID_REGEXP = /^(\w+(-(?=\w))?\w*)@\w+\.(\w+(-(?=\w))?\w+)$/;
 //const PASSWORD_VALID_REGEX = /^(\w|\W){8,}$/;
 const PASSWORD_VALID_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+const USERNAME_VALID_REGEX = /^[\d\w]+$/;
+const USERWISHES_VALID_REGEX = /^.+$/;
+const SELECTHOUSE_VALID_REGEX = /.+/;
 const VALID_CLASS = '--valid';
 const INVALID_CLASS = '--invalid';
 
@@ -10,13 +13,16 @@ const INVALID_CLASS = '--invalid';
 /*class for inputs*/
 class InputField {
     constructor(inputId, VALID_REGEXP){
-    this.inputId = inputId;
-    this.element = document.getElementById(this.inputId);
-    this.VALID_REGEXP = VALID_REGEXP;
+        this.inputId = inputId;
+        this.element = document.getElementById(this.inputId);
+        this.VALID_REGEXP = VALID_REGEXP;
     }
     /*return true if regexp match the input value*/
     isValid() {
-            return this.VALID_REGEXP.test(this.element.value);
+        if (this.element.nodeName === 'SELECT'){
+            return this.element.value !== '';
+        }
+        return this.VALID_REGEXP.test(this.element.value);
     };
     /*write valid/invalid class in input DOM elem*/
     viewValidation() {
@@ -58,16 +64,35 @@ function eventsLoader(){
         }
         ,{once:true});
 
+    let usernameInput = new InputField('right-panel__form-2__username', USERNAME_VALID_REGEX);
+    usernameInput.element.addEventListener('blur', () =>  {
+            usernameInput.viewValidation();
+            usernameInput.element.addEventListener('input', usernameInput.viewValidation.bind(usernameInput),{once:false});
+        }
+        ,{once:true});
+
+    let userWishesTextArea = new InputField('right-panel__form-2__userwishes', USERWISHES_VALID_REGEX);
+    userWishesTextArea.element.addEventListener('blur', () =>  {
+            userWishesTextArea.viewValidation();
+            userWishesTextArea.element.addEventListener('input', userWishesTextArea.viewValidation.bind(userWishesTextArea),{once:false});
+        }
+        ,{once:true});
+
+    let selectHouse = new InputField('selecthouse', SELECTHOUSE_VALID_REGEX);
+    selectHouse.element.addEventListener('blur', () =>  {
+            selectHouse.viewValidation();
+            selectHouse.element.addEventListener('input', selectHouse.viewValidation.bind(selectHouse),{once:false});
+        }
+        ,{once:true});
+
     const btnForm1 = document.getElementById('right-panel__form-1__submit-button');
-    /*btnForm1.addEventListener('submit', (event) => submitHandler(event, btnForm1, [emailInput, passwordInput]));*/
+    const btnForm2 = document.getElementById('right-panel__form-2__submit-button');
 
     const form1 = document.getElementById('right-panel__form-1');
     const form2 = document.getElementById('right-panel__form-2');
+    const form3 = document.getElementById('right-panel__form-3');
+
     form1.addEventListener('submit',(event) => submitHandler(event, btnForm1, [emailInput, passwordInput], () => changeSubmitedForm(form1,form2)));
+    form2.addEventListener('submit',(event) => submitHandler(event, btnForm2, [usernameInput, userWishesTextArea, selectHouse], () => changeSubmitedForm(form2,form3)));
 
 }
-
-
-
-
-
