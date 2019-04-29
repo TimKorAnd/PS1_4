7631f6dd9ad90fc17db7586b5fa41560ca18be66
timkor <timkorand@gmail.com>
2019-04-29 06:21:49 +0300

    mr pin,fonts,copypaste,comments fix

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const SELECTHOUSE_VALID_REGEX = /.+/;
 const VALID_CLASS = '--valid';
 const INVALID_CLASS = '--invalid';
 
-
 /*class for inputs*/
 class InputField {
     constructor(inputId, VALID_REGEXP, firstEventType, secondEventType, isShowPin = false){
@@ -19,9 +18,6 @@ class InputField {
         this.VALID_REGEXP = VALID_REGEXP;
         this.isShowPin = isShowPin;
         this.addEvents(firstEventType, secondEventType);
-        /*if (isShowPin)
-        this.element.addEventListener('focus',this.showPin.bind(this));*/
-
     }
     /*return true if regexp match the input value*/
     isValid() {
@@ -32,7 +28,6 @@ class InputField {
     };
     /*write valid/invalid class in input DOM elem*/
     viewValidation() {
-        /*if (this.isShowPin) {this.showPin();};*/
         let statusSearch = VALID_CLASS, statusReplace = INVALID_CLASS;
         if (this.isValid()) {
             [statusSearch, statusReplace] = [statusReplace, statusSearch];
@@ -43,7 +38,7 @@ class InputField {
         }
         this.element.className = this.element.className.replace(statusSearch, statusReplace);
         };
-    /**/
+    /*show pin for input from input's title*/
     showPin(displayPin) {
         if (this.element.parentElement.querySelector('.pin') != null && displayPin) {
             return;
@@ -54,7 +49,6 @@ class InputField {
             }
             return;
         }
-        console.log('showPinIsComming');
         let pinElem = document.createElement('div');
         let pinContent = document.createTextNode(this.element.title);
         let att = document.createAttribute('class');
@@ -63,7 +57,7 @@ class InputField {
         pinElem.appendChild(pinContent);
         this.element.parentNode.insertBefore(pinElem,this.element);
     }
-
+    /*add both events listeners to input instances*/
     addEvents(firstEventType,secondEventType) {
         this.element.addEventListener(firstEventType, () =>  {
                 this.viewValidation();
@@ -72,7 +66,7 @@ class InputField {
             ,{once:true});
     }
 }
-
+/*submit handler - form submited only if specified inputs is valid*/
 function submitHandler(event, btnSubmit, inputElems, submitFunc) {
     event.preventDefault();
     if (inputElems.every((currInputElem) => {
@@ -92,6 +86,7 @@ function eventsLoader(){
     /*get form1 DOM elems */
     let emailInput = new InputField('right-panel__form-1__email', EMAIL_VALID_REGEXP, 'blur','input');
     let passwordInput = new InputField('right-panel__form-1__password', PASSWORD_VALID_REGEX, 'blur', 'input',true);
+    /*get form2 DOM elems */
     let usernameInput = new InputField('right-panel__form-2__username', USERNAME_VALID_REGEX, 'blur', 'input');
     let userWishesTextArea = new InputField('right-panel__form-2__userwishes', USERWISHES_VALID_REGEX, 'blur', 'input');
     let selectHouse = new InputField('selecthouse', SELECTHOUSE_VALID_REGEX, 'blur', 'input');
@@ -103,7 +98,8 @@ function eventsLoader(){
     const form2 = document.getElementById('right-panel__form-2');
     const form3 = document.getElementById('right-panel__form-3');
 
-    form1.addEventListener('submit',(event) => submitHandler(event, btnForm1, [emailInput, passwordInput], () => changeSubmitedForm(form1,form2)));
-    form2.addEventListener('submit',(event) => submitHandler(event, btnForm2, [usernameInput, userWishesTextArea, selectHouse], () => changeSubmitedForm(form2,form3)));
-
+    form1.addEventListener('submit',(event) => submitHandler(event, btnForm1,
+        [emailInput, passwordInput], () => changeSubmitedForm(form1,form2)));
+    form2.addEventListener('submit',(event) => submitHandler(event, btnForm2,
+        [usernameInput, userWishesTextArea, selectHouse], () => changeSubmitedForm(form2,form3)));
 }
