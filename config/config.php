fc9c0e51dafca56c415f56dd481ef624c854fbc8
TimKorAnd <timkorand@gmail.com>
2019-11-12 23:45:20 +0200

    ps4 got file structure session test

--- config/config.php
+++ config/config.php
@@ -1,7 +1,8 @@
 <?php
 return [
     'reg-form' => [
-        'form-1-fields' => ['email','psw'], //name of voting,
+        'form-1-fields' => ['email','psw'],
+        'form-2-fields' => ['username','psw'],
         'cookieName' => 'isVoter', // generate smth safe/hash/personal ?
         'voteLifetime' => 60, //TODO change for '60*60*24'
         'candidateList' => ['zerocandidate','candidate1', 'candidate2', 'candidate3', 'four']
--- public/index.php
+++ public/index.php
@@ -1,17 +1,21 @@
+<?php session_start(['name'=>'sayMyName']) ?>
 <!DOCTYPE html>
 <html lang="en">
 <?php include_once('./html/header.html') ?>
 <body>
 <div class="container">
-  <?php include_once ('./html/great-house-logo-panel.html')?>
+  <?php /*include_once ('./html/great-house-logo-panel.html')*/?>
 <!--there are elems will created from js createHousesElemsInSlider($houseSlider);-->
 
   <section class="nav-panel">
     <?php /*if(isset(__SESSION[''])) */?>
-    <?php include_once ('./html/reg-form-1.php') ?>
+    <?php echo session_name()?>
+    <?php echo session_id()?>
 
-    <?php include_once ('./html/reg-form-2.php') ?>
-    <?php include_once ('./html/reg-form-3.php') ?>
+   <!-- <?php /*include_once ('./html/reg-form-1.php') */?>
+
+    <?php /*include_once ('./html/reg-form-2.php') */?>
+    --><?php /*include_once ('./html/reg-form-3.php') */?>
 
   </section>
 </div>
